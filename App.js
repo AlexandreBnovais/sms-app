d3a487d22f9cf5163ffb3442b0ed31dedd2c0c7b
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-10-06 21:37:24 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import react, {useEffect} from 'react';
+import { SafeAreaProvider} from 'react-native-safe-area-context';
+import { NavigationContainer} from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  useFonts
+} from '@expo-google-fonts/poppins';
